a6b6c4a0d90fca51e9ab0ff8066adae5a460ac5f
itmanbanana <itmanbanana@gmail.com>
2026-02-26 18:57:19 -0500

    Add search feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-console.log("Hello World!");
+const img = document.createElement("img");
+const key = process.env.GIPHY_KEY;
+const body = document.querySelector("body");
+const search = document.querySelector("input#search");
+const submitSearch = document.querySelector("button.search-button");
+submitSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchForImage(search.value);
+    search.value = "";
+});
+async function searchForImage(query) {
+    const url = `https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${query}`;
+    fetch(url)
+        .then(response => response.json())
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        .then((response) => {
+        console.log(response.data.images.original.url);
+        img.src = response.data.images.original.url;
+        body.appendChild(img);
+    })
+        .catch(error => console.error(error));
+}
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+const img: HTMLImageElement = document.createElement("img") as HTMLImageElement;
+const key = process.env.GIPHY_KEY;
+
+const body: HTMLBodyElement = document.querySelector("body") as HTMLBodyElement;
+const search: HTMLInputElement = document.querySelector("input#search") as HTMLInputElement;
+const submitSearch: HTMLButtonElement = document.querySelector("button.search-button") as HTMLButtonElement;
+
+submitSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchForImage(search.value);
+  search.value = "";
+});
+
+async function searchForImage(query: string) {
+  const url = `https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${query}`
+  fetch(url)
+    .then(response => response.json())
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    .then((response: any) => {
+      console.log(response.data.images.original.url);
+      img.src = response.data.images.original.url;
+      body.appendChild(img);
+    })
+    .catch(error => console.error(error));
+}
